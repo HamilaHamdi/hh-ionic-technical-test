6931533a76f7d862d6f36225b1ff74075a4d9df9
Hamdi Hamila <hmila.hmdi@gmail.com>
2018-05-13 12:37:03 +0100

    MITT-1 add posts list page , show posts v ia webservice, limit post body to 200 characters

--- src/Services/postsapi.service.ts
+++ src/Services/postsapi.service.ts
@@ -15,11 +15,11 @@ export class PostsApiService {
 
   constructor(private http: Http) { }
 
-    public getPosts(): Promise<any> {    // methode public qui implemente le service http
-    const url = `${this.baseUrl}`;       //  requette http
+    public getPosts(): Promise<any> {
+    const url = `${this.baseUrl}`;       //  affectation de l adresse de la requette http a la constante url
 
-    return this.http.get(url)             // recuperer le contenue d une requette http
-      .toPromise()                        // declaration d'une promesse ( apres avoir envoyer la requette je dois etre promis que je reçois une reponse )
+    return this.http.get(url)             // recuperer la reponse d une requette http
+      .toPromise()                        // conversion de la reponse de la requette en  Promise (apres avoir envoyer la requette je dois etre promis que je reçois une reponse )
       .then(response => response.json() as PostsApiList)                // reponse correcte recuprée  parser en objet json et caster en tant que  PostsApiList (model)
       .catch(error => console.log('Une erreur est survenue ' + error)) // reponse corrompue .. allerte a la console en tant qu' erreur
   }
